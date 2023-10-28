c12dbeb6b8a662dfa095c251bc1b93e68897189f
Alexander Martin <amart50@u.rochester.edu>
2023-10-28 14:28:08 -0400

    basic stats

---
+++ src/data_processing/statistics/README.md
@@ -0,0 +1,45 @@
+# Dataset Statistics 
+This folder contains the code to generate statistics for the dataset. 
+
+
+## Basic Statistics
+The basic statistics for the dataset are generated using the `basic_statistics.py` script. The script collects the statistics:
+1. Average number of tokens in the report and source text.
+2. The average number of filled roles across all frames. 
+3. The average number of filled roles for each individual frame.
+
+### Token Counts 
+Average Report Tokens: `58.95529411764706`
+Min Report Tokens: `10`
+Max Report Tokens: `526`
+Average Source Token: `1163.9843137254902`
+Min Source Tokens: `154`
+Max Source Tokens: `5553`
+
+### Role Counts
+For json outputs of the specific frame/role data see `sorted_norm_roles.json`
+Average Roles Across Frames: `3.015686274509804`
+
+Top 10 Frames with most roles (normalized by number of roles in a frame):
+1. `Subsisting`-> `1.0`
+2. `Transportation_status`-> `1.0`
+3. `Chaos`-> `0.95`
+4. `Vehicle_landing`-> `0.95`
+5. `Visiting`-> `0.95`
+6. `Coming_to_be`-> `0.9333333333333332`
+7. `Cause_to_make_progress`-> `0.9`
+8. `Detonate_explosive`-> `0.9`
+9. `Dodging`-> `0.9`
+10. `Event`-> `0.8666666666666667`
+
+Top 10 Frames with least roles (normalized by number of roles in a frame):
+1. `Killing`-> `0.39999999999999997`
+2. `Change_position_on_a_scale`-> `0.38`
+3. `Cause_to_be_sharp`-> `0.36`
+4. `Motion`-> `0.35555555555555557`
+5. `Cure`-> `0.35`
+6. `Change_of_leadership`-> `0.3333333333333333`
+7. `Proliferating_in_number`-> `0.275`
+8. `Cause_motion`-> `0.2727272727272727`
+9. `Apply_heat`-> `0.2285714285714286`
+10. `Education_teaching`-> `0.21538461538461537`
---
+++ src/data_processing/statistics/basic_stats.py
@@ -0,0 +1,176 @@
+"""
+basic statistics:
+average number of tokens in report/source
+average number/proportion of filled roles in report/source
+which frames have the least filled roles/most filled roles (maybe top 3 frames)
+"""
+
+import os 
+import sys
+import json
+import argparse
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+
+def token_counts(data: list):
+    """
+    Counts the average number of tokens in the report/source documents
+    """
+
+    report_token_counts = []
+    source_token_counts = []
+    min_report_size = sys.maxsize
+    min_source_size = sys.maxsize
+    max_report_size = -1
+    max_source_size = -1
+
+    # report token counts is length of report_dict[doctext-tok]
+    # source token counts is length of source_dict[doctext-tok]
+    for doc in tqdm(data):
+        report_len = len(doc["report_dict"]["doctext-tok"])
+        source_len = len(doc["source_dict"]["doctext-tok"])
+        if report_len < min_report_size:
+            min_report_size = report_len
+        elif report_len > max_report_size:
+            max_report_size = report_len
+
+        if source_len < min_source_size:
+            min_source_size = source_len
+        elif source_len > max_source_size:
+            max_source_size = source_len
+
+        report_token_counts.append(report_len)
+        source_token_counts.append(source_len)
+    
+    return (np.mean(report_token_counts), min_report_size, max_report_size), \
+            (np.mean(source_token_counts), min_source_size, max_source_size)
+
+def role_counts(data: list):
+    """
+    Counts the average number/proportion of filled roles in report/source
+
+    Returns:
+        a number (average number of filled roles across all frames) 
+        a dictionary (average number of filled roles per frame)
+    """
+    role_info = {} #has structure {frame: average: int, counts:{role: count}}}
+    for doc in tqdm(data):
+        if doc['frame'] not in role_info:
+            role_info[doc['frame']] = {'total': 0, 'average': 0, 'counts': {}}
+
+        # get number of filled roles
+        filled_roles = {} # has structure {role: 1/0}, 1 if filled, 0 if not
+        for role in doc['report_dict']['role_annotations']:
+            if role == 'role-spans-indices-in-all-spans':
+                continue
+            if doc['report_dict']['role_annotations'][role] != []:
+                filled_roles[role] = 1
+            else:
+                filled_roles[role] = 0
+
+        # update counts
+        for role in filled_roles:
+            if role not in role_info[doc['frame']]['counts']:
+                role_info[doc['frame']]['counts'][role] = 0
+            role_info[doc['frame']]['counts'][role] += filled_roles[role]
+
+        # update average
+        role_info[doc['frame']]['total'] += sum(filled_roles.values())
+
+    # divide each count by 5 (the number of docs) to get the average 
+    for frame in role_info:
+        role_info[frame]['average'] = role_info[frame]['total'] / 5
+        for role in role_info[frame]['counts']:
+            role_info[frame]['counts'][role] = role_info[frame]['counts'][role] / 5
+    
+    return role_info
+
+def normalize_roles(role_info: dict):
+    """
+    Normalize averages by number of roles in the frame
+    """
+    normalized_roles = {}
+    for frame in role_info:
+        normalized_roles[frame] = {'average': 0, 'counts': {}}
+        for role in role_info[frame]['counts']:
+            normalized_roles[frame]['counts'][role] = role_info[frame]['counts'][role]
+        normalized_roles[frame]['average'] = role_info[frame]['average'] / len(role_info[frame]['counts'])
+    return normalized_roles
+
+
+def main(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="data/cross_doc_role_extraction", help="directory of data") 
+    parser.add_argument("--dataset", type=str, default="all", help="dataset to analyze") # all, train, dev, test
+    parser.add_argument("--output_dir", type=str, default="src/data_processing/statistics", help="directory to save output")
+    parser.add_argument("--verbose", action="store_true", help="print out statistics")
+    args = parser.parse_args()
+
+    # load data
+    if args.dataset == "all":
+        data = []
+        for dataset in ["train", "dev", "test"]:
+            with open(os.path.join(args.data_dir, dataset + ".jsonl"), "r") as f:
+                for line in f:
+                    data.append(json.loads(line))
+
+    else:
+        with open(os.path.join(args.data_dir, args.dataset + ".jsonl"), "r") as f:
+            data = [json.loads(line) for line in f]
+
+    if args.verbose: print("Number of documents: {}".format(len(data)))
+
+    # get average number of tokens in report/source
+    report_data, source_data = token_counts(data)
+
+    if args.verbose: 
+        print("Average number of tokens in report: {}".format(report_data[0]))
+        print("Min Report Tokens: {}".format(report_data[1]))
+        print("Max Report Tokens: {}".format(report_data[2]))
+        print("Average number of tokens in source: {}".format(source_data[0]))
+        print("Min Source Tokens: {}".format(source_data[1]))
+        print("Max Source Tokens: {}".format(source_data[2]))
+
+    # get average number/proportion of filled roles in report/source
+    role_info = role_counts(data)
+
+    if args.verbose:
+        all_doc_average = 0
+        for frame in role_info:
+            all_doc_average += role_info[frame]['average']
+        print("Average number of filled roles across all frames: {}".format(all_doc_average / len(role_info)))
+
+    sorted_roles = sorted(role_info.items(), key=lambda x: x[1]['average'], reverse=True)
+
+    # if args.verbose:
+    #     print("Top 3 frames with the most filled roles:")
+    #     for frame in sorted_roles[:3]:
+    #         print(frame[0], frame[1]['average'])
+
+    #     print("Top 3 frames with the least filled roles:")
+    #     for frame in sorted_roles[-3:]:
+    #         print(frame[0], frame[1]['average'])
+
+    norm_roles = normalize_roles(role_info)
+
+    sorted_norm_roles = sorted(norm_roles.items(), key=lambda x: x[1]['average'], reverse=True)
+
+    if args.verbose:
+        print("Top 10 frames with the most normalized filled roles:")
+        for frame in sorted_norm_roles[:10]:
+            print(frame[0], frame[1]['average'])
+
+        print("Top 10 frames with the least normalized filled roles:")
+        for frame in sorted_norm_roles[-10:]:
+            print(frame[0], frame[1]['average'])
+
+    # write sorted norm roles to a json file
+    with open(os.path.join(args.output_dir, "sorted_norm_roles.json"), "w") as f:
+        json.dump(sorted_norm_roles, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()

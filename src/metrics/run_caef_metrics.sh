5bf664b1a19adcc43c1a42ae0aa0270b00bf56b5
Siddharth Vashishtha <sidsvash26@gmail.com>
2023-11-08 01:56:09 -0500

    added coref results + llama results

--- src/metrics/run_caef_metrics.sh
+++ src/metrics/run_caef_metrics.sh
@@ -1,3 +1,9 @@
 python compute_ceaf_metrics.py \
     --split "test" \
-    --output_dir "/data/svashishtha/FAMuS/src/metrics/"
+    --output_dir "/data/svashishtha/FAMuS/src/metrics/" \
+    --use_coref "yes"
+
+python compute_ceaf_metrics.py \
+    --split "test" \
+    --output_dir "/data/svashishtha/FAMuS/src/metrics/" \
+    --use_coref "no"

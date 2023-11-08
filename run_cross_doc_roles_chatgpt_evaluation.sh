7aae94378bcf198cdb8595bcf6e25ddc535bbb65
Siddharth Vashishtha <sidsvash26@gmail.com>
2023-11-08 00:38:36 -0500

    added metrics scripts

---
+++ run_cross_doc_roles_chatgpt_evaluation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Add the project root to PYTHONPATH
+export PYTHONPATH=.:$PYTHONPATH
+
+python src/model_scripts/test_chatgpt_on_cdae.py \
+        --input_dir "data/cross_doc_role_extraction/llm_prompt_format/" \
+        --output_dir "models/cdae/chatgpt/" \
+        --split "test" 

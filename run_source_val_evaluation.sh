11f277c0db60f7ec1fa3ce8a837d5e2b422f57e9
Siddharth Vashishtha <sidsvash26@gmail.com>
2023-11-26 12:08:26 -0500

    added source val model + instructions

--- run_source_val_evaluation.sh
+++ run_source_val_evaluation.sh
@@ -4,18 +4,18 @@ export PYTHONPATH=.:$PYTHONPATH
 
 python src/model_scripts/test_source_val.py \
         --input_data_dir "data/source_validation" \
-        --model_checkpoint_path "models/source_validation/results/best_model" \
+        --model_checkpoint_path "models/source_validation/longformer_source_val_best_model" \
         --chatgpt_output_file_path "models/source_validation/chatgpt/dev_gpt-3.5-turbo-0301_responses.jsonl" \
         --llama_output_file_path "models/source_validation/llama/dev_sv_llama_13b_responses.jsonl" \
         --split "dev" \
         --metrics_output_dir "src/metrics/" \
-        --gpu 2
+        --gpu 0
 
 python src/model_scripts/test_source_val.py \
         --input_data_dir "data/source_validation" \
+        --model_checkpoint_path "models/source_validation/longformer_source_val_best_model" \
         --chatgpt_output_file_path "models/source_validation/chatgpt/test_gpt-3.5-turbo-0301_responses.jsonl" \
         --llama_output_file_path "models/source_validation/llama/test_sv_llama_13b_responses.jsonl" \
-        --model_checkpoint_path "models/source_validation/results/best_model" \
         --split "test" \
         --metrics_output_dir "src/metrics/" \
-        --gpu 2
+        --gpu 0

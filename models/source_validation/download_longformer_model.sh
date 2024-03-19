2820c9eb3ce965908607034cd59804f1a0ecf004
Siddharth Vashishtha <sidsvash26@gmail.com>
2024-03-18 20:50:37 -0400

    added sv model download script

---
+++ models/source_validation/download_longformer_model.sh
@@ -0,0 +1,4 @@
+gdown https://drive.google.com/uc?id=15BqtuzKsWq5ox4ShcxE9_8HjIas2pQVB
+mv longformer_source_val_best_model.zip models/source_validation/
+# unzip the model into source_validation directory
+unzip models/source_validation/longformer_source_val_best_model.zip -d models/source_validation/

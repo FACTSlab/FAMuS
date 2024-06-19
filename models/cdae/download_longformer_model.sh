0cf226008303162ac8a90922e6e8cb3df63132a3
Siddharth Vashishtha <sidsvash26@gmail.com>
2024-06-18 20:09:11 -0400

    added cdae model links and updated readme

---
+++ models/cdae/download_longformer_model.sh
@@ -0,0 +1,4 @@
+gdown "https://drive.google.com/uc?id=1FCP5-tSV3ap1DnUNEH4akh5h77zvVuUf"
+mv longformer_cdae_qa_report_and_source_best_models.zip models/cdae/
+# unzip the model into cdae directory
+unzip models/source_validation/longformer_cdae_qa_report_and_source_best_models.zip -d models/cdae/
